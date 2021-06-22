607a070fa064675714cef61325452a9a462558dd
Sezem <86286280+Sezem@users.noreply.github.com>
2021-06-22 08:33:20 +0100

    Add files via upload

---
+++ src/components/Content.js
@@ -0,0 +1,194 @@
+import React from 'react';
+ 
+  function Content() {
+    return (
+       <div>
+         <section class="">
+              <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                     <img class="d-block w-100" src="down1.jpg" alt="First slide" style={{height:"auto"}}/>
+                  </div>
+                  <div class="carousel-item">
+                     <img class="d-block w-100 " src="Images/background.jpg" alt="Second slide" style={{height:"auto"}}/>
+                  </div>
+                  <div class="carousel-item">
+                     <img class="d-block w-100" src="Images/background.jpg" alt="Third slide" style={{height:"auto"}}/>
+                  </div>
+                </div>
+               </div>
+        </section>   
+ 
+        <section class="bg-light" id="about">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-12 col-md-12">
+                 <h3 class="text-center mt-4 text-secondary">About college of technology</h3>
+               </div>
+              </div>
+             <div class="row">
+                <p class="mt-4 mb-5">The College of Technology (COT) of the university of Buea was created as a component of the university by decree No.93/034 of 19 January 1993. The college is dedicated in providing high quality education, by TRAINING LOCALLY AND QUALIFYING GLOBALLY.
+
+Our vision is to offer both national and internationally recognized full and part-time educational programmes across a spectrum of engineering disciplines.
+
+We recognize that undergraduate education is a significant step towards enhancing your prospects and preparing you for future employment.
+
+Our courses are explicitly designed to train highly competent senior level technicians in relevant (up- to- date) engineering fields who are skilled in scientific research, team work and possess leadership skills.</p>
+             </div>   
+          </div>   
+        </section>   
+ 
+        <section class="" id="destinations">   
+         <div class="container">
+             <div class="row">
+              <div class="col-sm-12 col-md-12">
+                 <h3 class="text-center mt-4 text-secondary">course we offer</h3>
+               </div>
+              </div>
+              <div class="row">
+                 <div class="col-sm-4 mb-5">
+                    <div class="card mt-4">
+                       <img class="card-img-top" src="nnh.jpg" alt=""/>
+                       <div class="card-body">
+                          <h4 class="card-title text-secondary">computer</h4>
+                          <p class="card-text text-secondary">
+                             computer engineering is a department made up of three separate fields which are software engineering, cloud computing, and data sciences
+                          </p>
+                       </div>
+                       <div class="card-footer">
+                          <a href="#" class="btn btn-primary">Register for computer</a>
+                       </div>
+                    </div>
+                 </div>
+                 <div class="col-sm-4 mb-5">
+                    <div class="card mt-4">
+                       <img class="card-img-top" src="ggg.jpg" alt=""/>
+                       <div class="card-body">
+                          <h4 class="card-title text-secondary">Mechanical</h4>
+                          <p class="card-text text-secondary">Mechanical engineering is a department which studies the design,development,construction and testing of mechanical and thermal sensors and devices, including tools, engine anf machines</p>
+                       </div>
+                       <div class="card-footer">
+                          <a href="#" class="btn btn-primary">Regsiter for mechanical!</a>
+                       </div>
+                    </div>
+                 </div>
+                 <div class="col-sm-4 mb-5">
+                    <div class="card mt-4">
+                       <img class="card-img-top" src="j45.jpg" alt=""/>
+                       <div class="card-body">
+                          <h4 class="card-title text-secondary">Electrical</h4>
+                          <p class="card-text text-secondary">Electrical engineering is a department based on the field of power system and engineering.</p>
+                       </div>
+                       <div class="card-footer">
+                          <a href="#" class="btn btn-primary">Register for Electrical</a>
+                       </div>
+                    </div>
+                 </div>
+              </div>
+        </div>
+       </section>  
+ 
+ 
+       <section class=""  id="places">
+        <div class="container">
+          <div class="col-sm-12 col-md-12 mb-4">
+             <h3 class="text-center text-secondary mt-4">COT VISION</h3>
+         </div>
+       <div class="accordion" id="accordionExample">
+        <div class="card">
+         <div class="card-header" id="headingOne">
+            <h2 class="mb-0">
+            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+               Vision
+            </button>
+            </h2>
+         </div>
+ 
+         <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+            <div class="card-body">
+          The COT aims to be a benchmark technology institution not only by offering nationally and regionally recognized full-time and part-time aducational programmes across a spectrum of enginerring disciplines using traditional and leading edge teaching technologies and methods but also by pioneering innovative programmes, whose graduate will respond to the critical nation and sub-regional development needs and compete succesfully in the global job market.
+            </div>
+         </div>
+      </div>
+      <div class="card">
+         <div class="card-header" id="headingTwo">
+            <h2 class="mb-0">
+            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                
+            </button>
+            </h2>
+         </div>
+         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+            <div class="card-body">
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+         </div>
+      </div>
+      <div class="card">
+         <div class="card-header" id="headingThree">
+            <h2 class="mb-0">
+            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+      
+            </button>
+            </h2>
+         </div>
+         <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+            <div class="card-body">
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+         </div>
+      </div>
+      </div>
+    </div> 
+    </section>
+ 
+     
+    <section class="bg-light mt-5" id="tourist">    
+     <div class="container">
+      <div class="row text-center">
+        <div class="col-sm-12 col-md-12 mb-4">
+            <h3 class="text-center mt-4 text-secondary">Lecturers</h3>
+         </div>
+        <div class="col-md-4">
+          <div class="testimonial mb-5">
+           <div class="avatar mx-auto">
+            <img src="ssssh.jpg" class="rounded-circle z-depth-1 img-fluid"/>
+          </div>
+          <h4 class="font-weight-bold dark-grey-text mt-4">Dr. Michael Ekonde</h4>
+          <h6 class="font-weight-bold blue-text my-3"></h6>
+          <p class="font-weight-normal dark-grey-text">
+            Director</p>
+        </div>
+      </div>
+ 
+      <div class="col-md-4">
+        <div class="testimonial mb-5">
+          <div class="avatar mx-auto">
+            <img src="pph.jpg" class="rounded-circle z-depth-1 img-fluid"/>
+          </div>
+          <h4 class="font-weight-bold dark-grey-text mt-4">Dr Tchapga Christian</h4>
+          <h6 class="font-weight-bold blue-text my-3"></h6>
+          <p class="font-weight-normal dark-grey-text">Head of Internship</p>
+        </div>
+ 
+      </div>
+      <div class="col-md-4">
+        <div class="testimonial mb-5">
+          <div class="avatar mx-auto">
+            <img src="rrr.jpg" class="rounded-circle z-depth-1 img-fluid"/>
+          </div>
+          <h4 class="font-weight-bold dark-grey-text mt-4">Dr. Mih Thomas</h4>
+          <h6 class="font-weight-bold blue-text my-3"></h6>
+          <p class="font-weight-normal dark-grey-text">
+            H.O.D 
+            Computer Engineering</p>
+        </div>
+        </div>
+      </div>
+    </div> 
+   </section> 
+   </div>
+    );
+  }
+   
+  export default Content;
